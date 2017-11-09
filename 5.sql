8f493fc29bed0770096812382298774dfad0bd6f
hhlTer <32582929+hhlTer@users.noreply.github.com>
2017-11-09 19:38:14 +0200

    Update 5.sql

--- 5.sql
+++ 5.sql
@@ -3,23 +3,23 @@
 #-----------  прибыли компании (company) для каждой из компаний ------------------------------
 #---------------------------------------------------------------------------------------------
 
+CREATE TEMPORARY TABLE t1(
+SELECT mtm_id_company,
+name_company,
+name_cstmer,
+mtm_customer_id AS cstmr,
+sum(cost) as sumCost
+FROM  mtm_companies_projects
+JOIN mtm_customer_projects ON mtm_projects_id = mtm_id_projects
+JOIN projects ON projects.id = mtm_companies_projects.mtm_id_projects
+JOIN companies on companies.id = mtm_companies_projects.mtm_id_company
+JOIN customers on customers.id = mtm_customer_projects.mtm_customer_id
+GROUP BY mtm_customer_id, mtm_id_company, name_cstmer
+ORDER BY mtm_id_company
+);
+
 SELECT
   name_company,
   name_cstmer,
   min(t1.sumCost) AS msc
-
-FROM (
-                                 SELECT mtm_id_company,
-                                   name_company,
-                                   name_cstmer,
-                                   mtm_customer_id AS cstmr,
-                                   sum(cost) as sumCost
-                                 FROM  mtm_companies_projects
-                                   JOIN mtm_customer_projects ON mtm_projects_id = mtm_id_projects
-                                   JOIN projects ON projects.id = mtm_companies_projects.mtm_id_projects
-                                   JOIN companies on companies.id = mtm_companies_projects.mtm_id_company
-                                   JOIN customers on customers.id = mtm_customer_projects.mtm_customer_id
-                                 GROUP BY mtm_customer_id, mtm_id_company, name_cstmer
-                                 ORDER BY mtm_id_company
-                               ) t1
-GROUP BY name_company
+FROM t1 GROUP BY name_company;
