c27d7768304ec5f951f6302da13847db71047e05
hhlTer <32582929+hhlTer@users.noreply.github.com>
2017-11-09 12:04:45 +0200

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -2,13 +2,14 @@
 # 2. show the name of project, with have biggest price
 # ------------------------------------------------------
 
-SELECT projects.name_project, mtm_project_developers.mtm_id_project AS id_project,
-       sum(developers.salary) AS biggest_price
+SELECT  mtm_project_developers.mtm_id_project AS id_project,
+        projects.name_project AS costly_project,
+        sum(developers.salary) AS biggest_price
 FROM projects
 LEFT JOIN mtm_project_developers ON
           projects.id = mtm_project_developers.mtm_id_project
 LEFT JOIN developers ON
           mtm_project_developers.mtm_id_developer = developers.id
-GROUP BY mtm_id_project
-ORDER BY sum_salary DESC LIMIT 1;
+GROUP BY projects.name_project
+ORDER BY biggest_price DESC LIMIT 1;
 # ------------------------------------------------------
