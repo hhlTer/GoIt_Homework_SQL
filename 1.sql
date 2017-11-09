8417188bf5c531d3fc38da5198af9535d5fbb8ba
hhlTer <32582929+hhlTer@users.noreply.github.com>
2017-11-09 12:00:41 +0200

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -1,7 +1,7 @@
 ALTER TABLE developers ADD COLUMN
   salary DECIMAL UNSIGNED NOT NULL;
 
-UPDATE developers SET developers.salary = 1000;
+UPDATE developers SET developers.salary = 1000 WHERE id = 1;
 UPDATE developers SET developers.salary = 1500 WHERE id = 2;
 UPDATE developers SET developers.salary = 2000 WHERE id = 3;
 UPDATE developers SET developers.salary = 1200 WHERE id = 4;
